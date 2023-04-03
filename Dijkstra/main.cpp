fdf0c1291cde7b7af5adfe00bfe1a9ab6ceb03d0
Deniz Çetin <38467947+dnzngn@users.noreply.github.com>
2023-04-03 17:13:57 +0300

    Create main.cpp

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,46 @@
+#include "Dijkstra.h"
+
+int main() {
+    int n, m;
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    cout << "Enter the number of edges: ";
+    cin >> m;
+
+    int dist[n]; // kaç node varsa ona göre boş array oluşturuyor
+    //memset(dist, 0x3f, sizeof(dist)); // C dilinden kalma fonksiyon bellek ayrımı yapıyor
+    fill(dist, dist + n, INF); // C++11 ile geldi
+
+    vector<pair<int, int>> adj[n];
+
+    cout << "Enter the edges and their weights (node1 node2 weight): " << endl;
+    for (int i = 0; i < m; i++) {
+        int start, next, weight;
+        cin >> start >> next >> weight;
+        adj[start].emplace_back(next, weight); // sondan ekleme
+        //adj[v].emplace_back(u, w); // sondan ekleme - Tek yönlü hareket sağlanması için yoruma alındı
+    }
+
+    int src,dest;
+    cout << "Enter the source node: ";
+    cin >> src;
+
+    dijkstra(adj, dist, src); // Dijkstra.h'dan geliyor..
+
+    cout << "Enter the destination node: ";
+    cin >> dest;
+
+    cout << "Shortest distances from the source node to destination node: " << dist[dest] << '\n';
+
+    cout << "Shortest distances from the source node: " << endl;
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == 0x3f3f3f3f) { // INF yerine 0x3f3f3f3f kullanımı
+            cout << "unreachable" << endl;
+        } else {
+            cout << "Node " << i << ": " << dist[i] << endl;
+        }
+    }
+
+    return 0;
+}
