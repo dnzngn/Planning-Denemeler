f61338b1cb958c8d3b1ffe74f12f429f62fd94be
Deniz Çetin <38467947+dnzngn@users.noreply.github.com>
2023-04-03 17:13:37 +0300

    Create Dijkstra.h
    Dijkstra kütüphanesi tanımlandı

---
+++ Dijkstra/Dijkstra.h
@@ -0,0 +1,40 @@
+//
+// Created by dnz on 03.04.2023.
+//
+
+#ifndef DENEMELER_DIJKSTRA_H
+#define DENEMELER_DIJKSTRA_H
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+const int INF = INT_MAX;
+
+void dijkstra(vector<pair<int, int>> adj[], int dist[], int src) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // önceliğe göre büyükten küçüğe sıralayacak şekilde tanımlanıyor.
+    pq.emplace(0, src); // (0,0) ekle
+    dist[src] = 0; // Başlangıç konumu uzaklığı = 0
+
+    while (!pq.empty()) {
+        int u = pq.top().second; // değeri tutuyor ve en büyük weight değerini alıyor
+        pq.pop(); // daha sonra bu tutulan değeri çıkartıyor
+        
+
+        for (auto v : adj[u]) {
+            int neighbor = v.first; // adj[u] daki tutulan pair in ilk değeri
+            int weight = v.second; // adj[u] daki tutulan pair in ikinci değeri
+
+            if (dist[neighbor] > dist[u] + weight) {
+                dist[neighbor] = dist[u] + weight;
+                pq.emplace(dist[neighbor], neighbor);
+            }
+        }
+    }
+}
+
+#endif //DENEMELER_DIJKSTRA_H
